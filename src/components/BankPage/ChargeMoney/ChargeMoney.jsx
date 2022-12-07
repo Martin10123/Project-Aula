1a49c2e510674914453a726363710471721e5faa
Martin10123 <martinsimarra4@gmail.com>
2022-12-06 22:37:46 -0500

    Finalice la pagina principal del banco, donde ver las cuentas, tus transiciones y diferentes opciones, ahora hare las paginas de las opciones que se muestran ahi, empezando con la de ver el estado de mi cuenta

--- src/components/BankPage/ChargeMoney/ChargeMoney.jsx
+++ src/components/BankPage/ChargeMoney/ChargeMoney.jsx
@@ -1,74 +1,89 @@
-import { BiMailSend, BiLogOut } from "react-icons/bi";
-import { BsCreditCard, BsClipboardData } from "react-icons/bs";
-
-import { bankBackgro, bankMoney, photoUser } from "../../../images";
+import { AiOutlineDollarCircle } from "react-icons/ai";
+import { photoUser } from "../../../images";
+import { BiLogOut, BiTransfer } from "react-icons/bi";
+import { BsCreditCard, BsClipboardData, BsSafe2, BsBank } from "react-icons/bs";
 import { CardBank } from "./CardBank";
 import { FaRegBell } from "react-icons/fa";
 import { HiEllipsisVertical } from "react-icons/hi2";
+import { MdTransferWithinAStation } from "react-icons/md";
 
 import styles from "./chargeMoney.module.css";
 
 export const ChargeMoney = () => {
   return (
     <section className={styles.chargeMoney__container}>
+      <div className={styles.chargeMoney__div_color_back}>
+        <div className={styles.chargeMoney__inside_background}></div>
+      </div>
       <div className={styles.chargeMoney__content}>
         <nav className={styles.chargeMoney__nav}>
           <div className={styles.chargeMoney__image}>
             <img src={photoUser} alt="Foto de perfil usuario" />
-            <span className={styles.chargeMoney__span_name}>
-              <p>Martin Elias</p>
-              <p>Banco: Davivienda</p>
-            </span>
+            <p>Martin Elias</p>
           </div>
-          <div className={styles.chargeMoney__svgs_nav}>
-            <FaRegBell />
-            <HiEllipsisVertical />
-            <div className={styles.chargeMoney__options_ellip}>
-              <div className={styles.chargeMoney__back_trans}></div>
-              <div className={styles.chargeMoney__content_list_options}>
-                <div className={styles.chargeMoney__list_option}>
-                  <BsCreditCard />
-                  <p>Recargar</p>
-                </div>
-                <div className={styles.chargeMoney__list_option}>
-                  <BiMailSend />
-                  <p>Envia amigo</p>
-                </div>
-                <div className={styles.chargeMoney__list_option}>
-                  <BsClipboardData />
-                  <p>Ver datos</p>
-                </div>
-                <div className={styles.chargeMoney__list_option}>
-                  <BiLogOut />
-                  <p>Cerrar cesion</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <FaRegBell className={styles.chargeMoney__svgs_nav} />
         </nav>
 
         <div className={styles.chargeMoney__content_all_info}>
-          <div className={styles.chargeMoney__money_avaible}>
-            <h3>Montón disponible</h3>
-            <img src={bankMoney} alt="" />
-            <p>$2.556.000</p>
+          <div className={styles.chargeMoney__contains_money_list}>
+            <div className={styles.chargeMoney__money_avaible}>
+              <div className={styles.chargeMoney__name_bank}>
+                <p>
+                  Banco bogota <span>Card</span>
+                </p>
+                <p>Ver cuenta</p>
+              </div>
+              <div className={styles.chargeMoney__quantifi_money}>
+                <div className={styles.chargeMoney__spinner}></div>
+                <BsBank />
+                <p>Montón disponible</p>
+                <p>$2.556.000</p>
+              </div>
+            </div>
+
+            <div className={styles.chargeMoney__content_list_options}>
+              <div className={styles.chargeMoney__list_option}>
+                <BsCreditCard />
+                <p>Nueva tarjeta</p>
+              </div>
+              <div className={styles.chargeMoney__list_option}>
+                <AiOutlineDollarCircle />
+                <p>Recargar</p>
+              </div>
+              <div className={styles.chargeMoney__list_option}>
+                <MdTransferWithinAStation />
+                <p>Envia amigo</p>
+              </div>
+              <div className={styles.chargeMoney__list_option}>
+                <BsClipboardData />
+                <p>Ver datos</p>
+              </div>
+              <div className={styles.chargeMoney__list_option}>
+                <BsSafe2 />
+                <p>Caja fuerte</p>
+              </div>
+              <div className={styles.chargeMoney__list_option}>
+                <BiTransfer />
+                <p>Mis cuentas</p>
+              </div>
+            </div>
           </div>
-
-          <img
-            className={styles.chargeMoney__backgroup_transition}
-            src={bankBackgro}
-            alt=""
-          />
-
           <div className={styles.chargeMoney__transition}>
-            <h2>Tus transiciones</h2>
+            <h3>Tus transiciones</h3>
 
             <div className={styles.chargeMoney__list_transition}>
               <CardBank />
               <CardBank />
               <CardBank />
+              <CardBank />
             </div>
           </div>
+        </div>
+        <div className={styles.chargeMoney__box_button_logout}>
+          <button>
+            <BiLogOut />
+            Cerrar sesión
+          </button>
         </div>
       </div>
     </section>
---
+++ src/components/BankPage/SeeAccount/SeeAccount.jsx
@@ -0,0 +1,8 @@
+export const SeeAccount = () => {
+  return (
+    <div>
+      <h1>See account</h1>
+      <hr />
+    </div>
+  );
+};
